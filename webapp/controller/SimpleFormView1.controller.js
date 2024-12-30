1f79e2749349f71080d6cb78cae53d93eee807d1
Mayur Satardekar <mayursatardekar10@gmail.com>
2024-12-30 03:51:12 +0000

    Validation

--- webapp/controller/SimpleFormView1.controller.js
+++ webapp/controller/SimpleFormView1.controller.js
@@ -8,16 +8,38 @@ sap.ui.define([
         },
         onSaveNew(){
             var empid = this.getView().byId("EpIdIp1").getValue();
-
+// Employee ID is initial
             if (empid === "") {
                 this.getView().byId("EpIdIp1").setValueState("Error");
                 this.getView().byId("EpIdIp1").setValueStateText("Please enter employee Id");
             }
             else{
-                this.getView().byId("EpIdIp1").setValueState("None")
-                
+                this.getView().byId("EpIdIp1").setValueState("None")     
             }
+// Employee ID should be 10 characters
+          if (empid.length !== 10) {
+                this.getView().byId("EpIdIp1").setValueState("Error");
+                this.getView().byId("EpIdIp1").setValueStateText("Employee Id should be of 10 characters")
+            }
+            // else{
+            //     this.getView().byId("EpIdIp1").setValueState("None");
+            // }
+
+// Employee ID should be Alphabets/Numeric/AlphaNumeric
+// /^[a-zA-Z]+$/ = Alphabets
+// /^[0-9]+$/ = Numeric
+// /^[a-zA-Z0-9]+$/ = Alphanumeric
+            var regExp = /^[a-zA-Z0-9]+$/;
+            if(!empid.match(regExp)) {
+                this.getView().byId("EpIdIp1").setValueState("Error");
+                this.getView().byId("EpIdIp1").setValueStateText("Employee ID should be Alphabets");                
+            }
+            else{
+                this.getView().byId("EpIdIp1").setValueState("None");
+            }
+
         }
+
 
     });
 });
